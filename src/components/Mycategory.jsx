597719f382c7da6a5a0a044fb46d393d5ca00af4
L3gitmik3 <michael.munene.mwaniki@gmail.com>
2026-04-01 08:52:57 +0300

    updating the WebApp

--- src/components/Mycategory.jsx
+++ src/components/Mycategory.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import{BrowserRouter as Link} from 'react-router-dom'
+import{Link} from 'react-router-dom'
 
 const Mycategory = () => {
   return (
